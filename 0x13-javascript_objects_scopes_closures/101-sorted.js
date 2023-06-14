50c981e912ff218b701b648a12644a2b6325c8f4
Nneka-Edeh <treasuredneky@gmail.com>
2023-06-14 08:21:42 -0700

    Updated

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction){
-	for (let i = 0; i < x; i++) theFunction();
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
 }
+console.log(newDict);
