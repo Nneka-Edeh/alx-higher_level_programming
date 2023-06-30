91e0100553c44cede187d78a6e3e8b7a60a028d5
Nneka-Edeh <treasuredneky@gmail.com>
2023-06-30 01:30:44 -0700

    Updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,30 +1,3 @@
 #!/bin/bash
-
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-  echo "Please provide a URL."
-  exit 1
-fi
-
-# Store the URL in a variable
-URL="$1"
-
-# Send the request and store the response in a variable
-response=$(curl -sI "$URL")
-
-# Extract the content length from the response headers
-content_length=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')
-
-# If content length is empty, attempt to retrieve the size from the response body
-if [ -z "$content_length" ]; then
-  content_length=$(curl -s "$URL" | wc -c)
-fi
-
-# Check if the content length is still empty
-if [ -z "$content_length" ]; then
-  echo "Unable to retrieve the content length."
-  exit 1
-fi
-
-# Display the content length in bytes
-echo "$content_length"
+# A bash Script that displays the Content-Length from a HTTP request
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
